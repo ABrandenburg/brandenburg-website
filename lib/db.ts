d2c71d62c1ddc28c3c825a280f8b21959814fcd5
Adam Brandenburg <65125938+ABrandenburg@users.noreply.github.com>
2026-01-19 16:33:08 -0500

    v1.5: Add explicit SSL requirement for Supabase

--- lib/db.ts
+++ lib/db.ts
@@ -6,5 +6,9 @@ import * as schema from './schema'
 const connectionString = process.env.DATABASE_URL!
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
-const client = postgres(connectionString, { prepare: false })
+// Add ssl: 'require' for Supabase connections
+const client = postgres(connectionString, {
+    prepare: false,
+    ssl: 'require'
+})
 export const db = drizzle(client, { schema })
